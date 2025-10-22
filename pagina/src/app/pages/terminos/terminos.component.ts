1e995cb2c73afbae9db22cf23bc22c1258931ea2
gustavo palomino <gpalomino9690@gmail.com>
2025-10-22 03:57:39 -0500

    tercera carga

--- pagina/src/app/pages/terminos/terminos.component.ts
+++ pagina/src/app/pages/terminos/terminos.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-terminos',
+  standalone: true,
   imports: [],
   templateUrl: './terminos.component.html',
-  styleUrl: './terminos.component.scss'
+  styleUrls: ['./terminos.component.scss']
 })
 export class TerminosComponent {
 
